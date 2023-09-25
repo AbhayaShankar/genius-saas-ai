7043651563785ce7775ea82d2d25c71985eb3607
Abhaya <shankarabhaya9@gmail.com>
2023-09-25 21:03:50 +0530

    clerk auth added

--- app/(dashboard)/(landing)/page.tsx
+++ app/(dashboard)/(landing)/page.tsx
@@ -1,7 +1,23 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { Fragment } from "react";
+
 export default function LandingPage() {
   return (
-    <>
-    <p className="text-2xl text-slate-500 font-bold">Landing Page (Unprotected)</p>
-    </>
+    <Fragment>
+        <p className="text-2xl text-slate-500 font-bold">Landing Page (Unprotected)</p>
+        <div>
+            <Link href={"/sign-in"}> 
+                <Button>
+                    Login
+                </Button>
+            </Link>
+            <Link href={"/sign-up"}> 
+                <Button>
+                    Register
+                </Button>
+            </Link>
+        </div>
+    </Fragment>
   )
 }
