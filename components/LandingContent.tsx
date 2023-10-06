9fa506591c4d98143bd59bb521352edae04a91dc
Abhaya <shankarabhaya9@gmail.com>
2023-10-06 20:36:08 +0530

    Landing layout 50% done

---
+++ components/LandingContent.tsx
@@ -0,0 +1,5 @@
+const LandingContent = () => {
+  return <div className="text-white">LandingContent</div>;
+};
+
+export default LandingContent;
--- components/LandingHero.tsx
+++ components/LandingHero.tsx
@@ -1,9 +1,13 @@
 "use client";
 
+import { useAuth } from "@clerk/nextjs";
+import Link from "next/link";
 import React from "react";
 import TypewriterComponent from "typewriter-effect";
+import { Button } from "./ui/button";
 
 const LandingHero = () => {
+  const { isSignedIn } = useAuth();
   return (
     <div className="text-white font-bold py-12 md:py-28 text-center space-y-5">
       <div className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-extrabold space-y-5">
@@ -23,6 +27,27 @@ const LandingHero = () => {
             }}
           />
         </div>
+      </div>
+      <div className="text-sm md:text-lg font-semibold text-zinc-500 tracking-wide">
+        Create content using AI 10x faster.
+      </div>
+      <div>
+        <Link href={isSignedIn ? "/dashboard" : "/sign-up"}>
+          <Button
+            variant={"premium"}
+            className="p-4 md:p-6 md:text-lg rounded-lg tracking-wide"
+          >
+            Start Generating for FREE
+          </Button>
+        </Link>
+      </div>
+      <div className="text-sm md:text-md font-semibold text-zinc-500 tracking-wide">
+        No Credit Card Required. Test for{" "}
+        <span className="text-transparent bg-clip-text bg-gradient-to-r  from-purple-600 to-pink-600">
+          {" "}
+          Free
+        </span>
+        .
       </div>
     </div>
   );
